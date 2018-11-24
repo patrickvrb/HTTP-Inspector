605af9649926fd7da735632bc09798672c686b2e
Patrick Beal <patrickvrb@gmail.com>
2018-11-24 16:23:23 -0200

    Basic server socket cpp file

---
+++ server_socket.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#define PORT 8228 /* Porta  Proxy Local*/
+
+int main(int argc, char const *argv[])
+{
+    printf("Mensagem Recebida\n");
+    printf("Mensagem Enviada\n");
+    return 0;
+}
