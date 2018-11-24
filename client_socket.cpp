8eeae3f4e250b01b6814b36d4e8a999120c63359
Dans1997 <danillo.neves97@gmail.com>
2018-11-24 17:18:37 -0200

    Basic client socket + error messages & send

--- client_socket.cpp
+++ client_socket.cpp
@@ -5,6 +5,39 @@
 
 int main(int argc, char const *argv[])
 {
+    struct sockaddr_in address;
+    struct sockaddr_in serv_addr;
+
+    int sock = 0, valread;
     char const *hello = "O cliente diz oi!";
-    
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Criacao do socket falhou \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    /* Converter enderecos IPv4 and IPv6 para binario */
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nEndereco invalido/ nao suportado \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConexao falhou \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Mensagem enviada!\n");
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );
+    return 0;
 }
