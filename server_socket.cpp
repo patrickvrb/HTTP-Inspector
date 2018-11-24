4889f0e33da5031a6dfe548612df55c28ba48fea
Patrick Beal <patrickvrb@gmail.com>
2018-11-24 16:43:45 -0200

    Create server socket function

--- server_socket.cpp
+++ server_socket.cpp
@@ -3,10 +3,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#define PORT 8228 /* Porta  Proxy Local*/
+#define PORT 8228 /* Porta Proxy Local 8228*/
 
 int main(int argc, char const *argv[])
 {
+
+    int server_socket;
+
+        /* Criando o Socket */
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        printf("Socket Falhou!\n");
+    }
+    else printf("Socket Criado com Sucesso!\n");
+
+
+
+
+
     printf("Mensagem Recebida\n");
     printf("Mensagem Enviada\n");
     return 0;
