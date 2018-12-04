0fe61de86465b8e63c62a62c580431cdece59599
Patrick Beal <patrickvrb@gmail.com>
2018-12-03 23:22:41 -0200

    Requisitions Updated (DAN)
    Co-authored-by: Dans1997 <danillo.neves97@gmail.com>

--- proxy_server.cpp
+++ proxy_server.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <iostream>
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -13,11 +15,19 @@
 
 #define PORT 8228 /* Porta Proxy Local 8228 */
 #define BUFFERSIZE 4096
-#define BUFFEROP 200000000
+#define BUFFEROP 4096
 
 char *server_response(char[]);
 int get_ip(char *, char *);
+char *load_cache(char *r, int );
+int save_cache(char *, int);
+int fileSearch(char *, FILE *);
+void sendBrowser(char *);
+int findMethod(char *str);
+
 using namespace std;
+
+int cache_cont = 1;
 
 int main(int argc, char const *argv[])
 {
@@ -49,12 +59,11 @@ int main(int argc, char const *argv[])
           printf("Não foi possível escutar!\n");
           exit(1);
      }
-     printf("Escutando...\n");
 
      int browser_conn, message;
      char request[BUFFERSIZE], *response;
 
-     response = (char *)malloc(BUFFEROP);
+     response = (char *)malloc(BUFFEROP*sizeof(char));
 
      while (1)
      {
@@ -68,26 +77,100 @@ int main(int argc, char const *argv[])
           else
           {
                printf("Conexao aceita! \n");
-               //Receber data do Browser
+               //Receber Request do Browser
                memset(&request, '\0', sizeof(request));
                message = recv(browser_conn, request, sizeof(request), 0);
                if (message < 0)
                {
                     perror("Leitura");
+               } else {
+                    printf("Pedido do Browser:\n%s\n", request);
+                    // strcpy(response, server_response(request));
+                    response = server_response(request);
+                    printf("Resposta do Servidor:\n%s\n", response);
+                    if (save_cache(response, 0) != 0 )
+                    {
+                         printf("Erro de cache\n");
+                         exit(-1);
+                    } 
+                    message = send(browser_conn, response, sizeof(response), 0);
+                    if (message < 0)
+                    {
+                         perror("Escrita");
+                    }
+                    //sendBrowser(response);
+                    printf("\nEscutando...\n");
                }
-               else
-               {
-                    printf("Pedido do Browser: %s\n", request);
-                    strcpy(response, server_response(request));
-                    printf("Resposta do server: %s", response);
-                    free(response);
-               }
+               fflush(stdin);
                fflush(stdout);
           }
      }
+     //free(response);
      close(browser_conn);
 
      return 0;
+}
+
+void sendBrowser(char *str){
+
+     char *http_data, c = str[0];
+     http_data = (char *) malloc(BUFFERSIZE*sizeof(char));
+
+     int i = 1, j = 2;
+
+     while (c != '<')
+     {
+          c = str[i++];
+          //printf("Char: %c\n", c);
+     }
+     http_data[0] = c;
+     c = str[i++];
+     http_data[1] = c;
+     while (c != '')
+     {
+          c = str[i++];
+          http_data[j++] = c;
+         
+     }
+     http_data[j++] = '\0';
+
+     char http_header[BUFFERSIZE] = "HTTP/1.1 400 Bad Request\r\n\n";
+     strcat(http_header, http_data);
+
+
+     int proxy_socket, sockopt = 1;
+
+     if ((proxy_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+     {
+          /* IPV4, TCP, IP = 0 */
+          printf("Socket do Browser sFalhou!\n");
+     }
+     else
+          printf("Socket do Browser Criado com Sucesso!\n");
+
+     setsockopt(proxy_socket, IPPROTO_TCP, TCP_NODELAY, (const char *)&sockopt, sizeof(int));
+
+     struct sockaddr_in browser_address;
+     browser_address.sin_family = AF_INET;
+     browser_address.sin_port = htons(5000);
+     browser_address.sin_addr.s_addr = INADDR_ANY;
+
+     if (connect(proxy_socket, (struct sockaddr *)&browser_address, sizeof(struct sockaddr_in)) == -1)
+     {
+          perror("Erro ao conectar com o Browser:");
+          exit(1);
+     }
+     printf("Browser conectado\n\n");
+
+     int browser_conn, message;
+
+     message = write(proxy_socket, http_header, sizeof(http_header));
+     if (message < 0)
+     {
+          perror("Erro no envio ao Browser");
+     } else {
+          printf("Pedido Encaminhado ao Browser\n");
+     }     
 }
 
 char *server_response(char *request)
@@ -150,15 +233,26 @@ char *server_response(char *request)
           perror("Connection Error:");
           exit(1);
      }
-
-     printf("CONECTOU PORRA !!!!\n");
+     printf("Servidor conectado\n\n");
 
      char *response;
      response = (char*)malloc(sizeof(BUFFEROP));
 
      //BOTANDO O REQUEST NO FORMATO PADRÃO 
      
-     strcat(newrequest, "GET http://");
+     switch(findMethod(request)){
+          case 0:
+               strcat(newrequest, "GET http://");
+               break;
+          case 1:
+               strcat(newrequest, "POST http://");
+               break;
+          case -1:
+               perror("Erro! Nem GET nem POST!\n");
+               exit(-1);
+               break;
+     }
+     
      strcat(newrequest, hostname);
      strcat(newrequest, "/ HTTP/1.1\r\n\r\n");
 
@@ -167,11 +261,21 @@ char *server_response(char *request)
      //send(proxy_server_socket, request, sizeof(request), 0);
      //recv(proxy_server_socket, &response, sizeof(response), 0);
 
-     printf("PASSOU RECEIV !!!!\n");
+     close(proxy_server_socket);
 
-     //close(proxy_server_socket);
+     //load_cache(response, 0);
 
      return response;
+}
+
+int findMethod(char *str){
+     if(str[0] == 'P'){
+          return 1;
+     } else if(str[0] == 'G') {
+          return 0;
+     } else {
+          return -1;
+     }
 }
 
 int get_ip(char *hostname, char *ip)
@@ -193,7 +297,121 @@ int get_ip(char *hostname, char *ip)
      return 1;
 }
 
-void cache(char *request)
+int save_cache(char *str, int mode)
 {
      FILE *cache;
+
+     cache = fopen("cache.txt","a+");
+     if(cache == NULL){
+          printf("Erro ao abrir o arquivo\n");
+          return -1;
+     }
+
+     switch(mode){
+          case 0:
+               fprintf(cache, "Segmento No. %d\n", cache_cont++);
+               fprintf(cache, "%s", str);
+               fprintf(cache, "\n\n");
+
+               return 0;
+               break;
+          default:
+               printf("Modo cache inexistente\n");
+               return -1;
+               break;
+     }
+
+     fclose(cache);
+}
+
+char *load_cache(char *str, int mode)
+{
+     FILE *cache;
+
+     cache = fopen("cache.txt","a+");
+     if(cache == NULL){
+          printf("Erro ao abrir o arquivo\n");
+          exit(-1);
+     }
+
+     char *http_data, c = str[0];
+     http_data = (char *) malloc(BUFFERSIZE*sizeof(char));
+
+     int i = 1, j = 2, aux;
+
+     while (c != '<')
+     {
+          c = str[i++];
+          //printf("Char: %c\n", c);
+     }
+     http_data[0] = c;
+     c = str[i++];
+     http_data[1] = c;
+     while (c != '')
+     {
+          c = str[i++];
+          http_data[j++] = c;
+         
+     }
+     http_data[j++] = '\0';
+     //strtok(hostname, "/");
+
+     switch(mode){
+          case 0:
+               if(fileSearch(http_data, cache) == 0)
+               {
+                    return http_data;
+                    break;
+               } else {
+                    printf("Nada no cache\n");
+                    char *nothing;
+                    return nothing;
+               }
+          default:
+               printf("Modo cache inexistente\n");
+               char *nothing;
+               return nothing;
+               break;
+     }
+
+     fclose(cache);
+}
+
+int fileSearch(char *str, FILE *fp) {
+
+	char str_c, file_c;
+     int str_len, i = 1, flag = 1;
+
+     str_len = strlen(str);
+     //printf("Tamanho: %d\n", str_len);
+
+     printf("String: %s\n", str);
+     
+     while( !feof(fp) ) {
+          fread(&file_c,1, 1, fp);
+          str_c = str[0];
+          while(str_c != file_c)
+          {
+               fread(&file_c, 1, 1, fp);
+          }
+
+          printf("Iguais: %c e %c\n", str_c, file_c);
+
+          while(flag == 1)
+          {
+               fread(&file_c, 1, 1, fp);
+               str_c = str[i++];
+               printf("String: %c e File: %c\n", str_c, file_c);
+               if(str_c != file_c){
+                    flag = 0;
+                    break;
+               } 
+               else
+               {
+                    if(i == str_len) return 0;
+               }
+          }
+          break;
+     }
+     return -1;
 }
